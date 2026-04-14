a8ee08d0672a2a4ed05cd2c9a28c13d3738b96ac
PentagonCoder <harshu33908@gmail.com>
2026-04-14 14:16:18 +0000

    Remove unnecessary setInterval for broadcasting on client connection

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ wss.on('connection', (ws, request) => {
 
   // add client
   clients.push(ws);
-  setInterval(() => {
+  
     broadcast()   
-  }, 50);
+ 
 
   // when message comes
   ws.on('message', (data) => {
